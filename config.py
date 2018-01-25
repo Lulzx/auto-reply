fdba6fde9d3a81db3c2d23788010444c507ce5ac
lulzx <wiserishabh@gmail.com>
2018-01-25 17:05:54 +0530

    done neccesary things

---
+++ config.py
@@ -0,0 +1,2 @@
+api_id = 12345 api_hash = "xxx" phone = "xxx"
+session_id = "string"
--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 from telethon import TelegramClient from 
 telethon.tl.types import UpdateShortMessage import 
-config import time import messagefilters import 
-functions client = TelegramClient(config.session_id, 
-config.api_id,
-                        config.api_hash, 
-update_workers=4) client.connect() if not 
-client.is_user_authorized():
+config, time, messagefilters, functions client = 
+TelegramClient(config.session_id, config.api_id, 
+config.api_hash, update_workers=4) client.connect() 
+if not client.is_user_authorized():
     client.sign_in(config.phone)
-    client.sign_in(code=input('Code:')) print 
-len(client.get_dialogs()) def printupdate(update):
-    print update
+    client.sign_in(code=input("Code:")) 
+print(len(client.get_dialogs())) def 
+printupdate(update):
+    print(update)
     if messagefilters.Filters.command(update) and 
-isinstance(update,
-            UpdateShortMessage):
+isinstance(update, UpdateShortMessage):
         text = update.effective_message.message
-        if text == '/start':
-            print '/start command used'
+        if text == "/start":
+            print("/start command used")
             try:
-                print 
-functions.effective_chat_id(update)
                 
-client.send_message(functions.effective_chat_id(update)[1],
-                                    'message')
-            except Exception, e:
-                print e 
-client.add_update_handler(printupdate)
+print(functions.effective_chat_id(update))
+                
+client.send_message(functions.effective_chat_id(update)[1], 
+"hello, world")
+            except Exception as e:
+                print(e) 
+client.add_update_handler(printupdate) 
 time.sleep(1000000)
