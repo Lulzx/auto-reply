3ab21fc4bb8f2423b559209925d5051068db3954
Lulzx <wiserishabh@gmail.com>
2018-01-25 17:09:02 +0530

    Ahh

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-api_id = 12345 api_hash = "xxx" phone = "xxx"
-session_id = "string"
-=======
 api_id = 12345
 api_hash = "xxx"
 phone = "xxx"
 session_id = "string"
->>>>>>> 2bcaa8d1a6a8c6225e9dc44c3d870dbca6c716bc
